37c2dc53c8b6a31965e2b517d6d80037d8907018
Aleksandar70 <aleksandar.berdovic@gmail.com>
2021-08-03 19:14:57 +0200

    Added constants

--- constants/constants.ts
+++ constants/constants.ts
@@ -3,6 +3,9 @@ export abstract class Constants {
   static readonly GTYPE_LOCAL: string = "local";
   static readonly GTYPE_REMOTE: string = "remote";
   static readonly GTYPE_SEMI: string = "semi";
+  static readonly PERFORMANCE_DNF: string = "DNF";
+  static readonly PERFORMANCE_DQ: string = "DQ";
+  static readonly PERFORMANCE_DNS: string = "DNS";
 
   static readonly DEFAULT_COMPETITION_YEAR: number = 2021;
 }
