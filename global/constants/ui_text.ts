40b7420250609610697012f3817088aab09418af
Aleksandar70 <aleksandar.berdovic@gmail.com>
2021-08-12 23:13:40 +0200

    Remove constant

--- global/constants/ui_text.ts
+++ global/constants/ui_text.ts
@@ -1,6 +1,5 @@
 export abstract class UIText {
   static readonly COLUMN_TOGGLE: string = "Add / Remove Columns";
   static readonly TABLE_SAVE: string = "Save table";
-  static readonly SOURCES: string = "Sources";
   static readonly SOURCES_HEADER: string = "Pick one source";
 }
