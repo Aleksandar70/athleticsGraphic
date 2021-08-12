0947e84d4534f696e56e630a274c86c7abff714d
Aleksandar70 <aleksandar.berdovic@gmail.com>
2021-08-12 18:36:18 +0200

    Add dropdown menu

--- src/components/canvas/Canvas.svelte
+++ src/components/canvas/Canvas.svelte
@@ -10,8 +10,9 @@
   } from "../data_table/table.helper";
   import "./canvas.style.css";
   import { updateCompetitors } from "../../api/competitor.api";
-  import { Button } from 'sveltestrap';
+  import { Button, Dropdown } from "sveltestrap";
 
+  let dropdownTrigger;
   export let tableData;
 
   let headerData = getHeaderData(tableData);
@@ -22,6 +23,20 @@
   <Modal>
     <ColumnDisplayOptions bind:headerData bind:rowData />
   </Modal>
+  <Dropdown triggerElement={dropdownTrigger}>
+    <Button
+      type="button"
+      class="btn btn-secondary dropdown-toggle"
+      bind:this={dropdownTrigger}
+    >
+      Dropdown
+    </Button>
+    <div slot="DropdownMenu">
+      <Button class="dropdown-item" type="button">Action</Button>
+      <Button class="dropdown-item" type="button">Another action</Button>
+      <Button class="dropdown-item" type="button">Something else here</Button>
+    </div>
+  </Dropdown>
   <Button on:click={() => getModal().open()}>{UIText.COLUMN_TOGGLE}</Button>
   <DataTable {headerData} {rowData} />
   <Button
