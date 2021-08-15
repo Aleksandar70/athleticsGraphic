12c7b1ac6227d3da89d1e25141dbb0c0e1ad9ac2
Aleksandar70 <aleksandar.berdovic@gmail.com>
2021-08-15 08:27:21 +0200

    Added logic for pagination

--- src/components/pagination/TablePagination.svelte
+++ src/components/pagination/TablePagination.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
   import { Pagination, PaginationItem, PaginationLink } from "sveltestrap";
+  let rows = [];
+  let page = 0;
+  let totalPages = [];
+  let currentPageRows = [];
+  let itemsPerPage = 5;
+  let loading = true;
+
+  $: currentPageRows = totalPages.length > 0 ? totalPages[page] : [];
+  const setPage = async (page) => {
+    if (page >= 0 && page < totalPages.length) {
+    }
+  };
+
+  const paginate = (items) => {
+    const pages = Math.ceil(items.length / itemsPerPage);
+
+    const paginatedItems = Array.from({ length: pages }, (_, index) => {
+      const start = index * itemsPerPage;
+      return items.slice(start, start + itemsPerPage);
+    });
+
+    console.log("paginatedItems are", paginatedItems);
+    totalPages = [...paginatedItems];
+    currentPageRows = paginatedItems[page];
+  };
 </script>
 
 <Pagination size="sm">
   <PaginationItem>
-    <PaginationLink first href="#" />
+    <PaginationLink first on:click={() => setPage(page)} />
   </PaginationItem>
   <PaginationItem>
-    <PaginationLink previous href="#" />
+    <PaginationLink previous on:click={() => setPage(page - 1)} />
+  </PaginationItem>
+  {#each totalPages as page, i}
+    <PaginationItem>
+      <PaginationLink>{i + 1}</PaginationLink>
+    </PaginationItem>
+  {/each}
+  <PaginationItem>
+    <PaginationLink next on:click={() => setPage(page + 1)} />
   </PaginationItem>
   <PaginationItem>
-    <PaginationLink href="#">1</PaginationLink>
-  </PaginationItem>
-  <PaginationItem>
-    <PaginationLink href="#">2</PaginationLink>
-  </PaginationItem>
-  <PaginationItem>
-    <PaginationLink href="#">3</PaginationLink>
-  </PaginationItem>
-  <PaginationItem>
-    <PaginationLink next href="#" />
-  </PaginationItem>
-  <PaginationItem>
-    <PaginationLink last href="#" />
+    <PaginationLink last on:click={() => setPage(page)} />
   </PaginationItem>
 </Pagination>
