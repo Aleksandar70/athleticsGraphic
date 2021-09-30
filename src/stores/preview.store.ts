a62f5fccfbb008789cd43a1a423dedc5bfc1d0b4
Aleksandar70 <aleksandar.berdovic@gmail.com>
2021-09-30 15:30:28 +0200

    Remove clear

--- src/stores/preview.store.ts
+++ src/stores/preview.store.ts
@@ -3,7 +3,4 @@ import { readable, writable } from "svelte/store";
 const channel = new BroadcastChannel("preview");
 export const previewChannel = readable(channel);
 
-const clear = new BroadcastChannel("clear");
-export const clearChannel = readable(clear);
-
 export const visiblePreview = writable({ id: "", data: {}, type: undefined });
